0af9f5eeec79663460209768aa0b59ea4cae2b9c
Koder <43494256+Koder0205@users.noreply.github.com>
2019-10-14 23:52:51 +0900

    Create RobotArm.ino
    로봇팔 소스코드.
    나머지 세개의 축에 대한 소스코드 작성하고
    하드웨어에 옮겨서 테스트후 완성

---
+++ RobotArm/RobotArm.ino
@@ -0,0 +1,50 @@
+#include <Servo.h>
+
+#define e0clk 2
+#define e0dt  3
+#define e1clk 4
+#define e1dt  5
+#define e2clk 6
+#define e2dt  7
+#define e3clk 8
+#define e3dt  9
+
+#define sp0 10
+#define sp1 11
+#define sp2 12
+#define sp3 13
+
+int e0last = LOW ,e1last = LOW ,e2last = LOW ,e3last = LOW;
+int e0n = LOW, e1n = LOW, e2n = LOW, e3n = LOW;
+int e0pos, e1pos, e2pos, e3pos;
+Servo s0,s1,s2,s3;
+
+ void setup() {
+   for(int i=2; i<=9; i++)pinMode(i,INPUT);
+   for(int i=10; i<=13; i++)pinMode(i, OUTPUT);
+   s0.attach(sp0);
+   s1.attach(sp1);
+   s2.attach(sp2);
+   s3.attach(sp3);
+   Serial.begin (9600);
+ } 
+
+ void loop() { 
+   e0n = digitalRead(e0clk);
+   e1n = digitalRead(e1clk);
+   e2n = digitalRead(e2clk);
+   e3n = digitalRead(e3clk);
+
+   //예로 소스코드 통일해줄것.(나머지 추가)
+   if ((e0last == LOW) && (e0n == HIGH)) {
+     if (digitalRead(e0dt) == LOW) e0pos-=5;
+     else e0pos+=5;
+     s0.write(abs(e0pos) % 180);
+     Serial.println (e0pos);
+   }
+   
+   e0last = e0n;
+   e1last = e1n;
+   e2last = e2n;
+   e3last = e3n;
+ } 
